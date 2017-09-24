fb2ac4584537fc9a6ba9ce11ad55b6b272e2811f
beakerandjake <jhoff462@gmail.com>
2017-09-24 10:16:48 -0700

    adding file/css loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            // STYLES
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            // IMAGES
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        }, {
+            // FONTS
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            loader: 'file-loader'
+        }]
+    }
 };
