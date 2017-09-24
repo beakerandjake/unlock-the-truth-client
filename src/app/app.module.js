a8f8ee35a7554c3015ff6c5c6a2108921d15fc62
beakerandjake <jhoff462@gmail.com>
2017-09-24 12:05:36 -0700

    set html5Mode

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,6 +4,10 @@ const moduleName = 'unlock-the-truth';
 
 //Declare our main module.
 angular
-    .module(moduleName, []);
+    .module(moduleName, [])
+    .config(['$locationProvider', $locationProvider => {
+        // Configure app to have pretty urls (no #)
+        $locationProvider.html5Mode(true);
+    }]);
 
 export default moduleName;
