45fdac049d7d7b59c44ba98823d9ec90953b5689
beakerandjake <jhoff462@gmail.com>
2017-09-24 10:22:30 -0700

    wip webpack clean/html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/app/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: "HTML"
+        })
+    ],
     module: {
         rules: [{
             // STYLES
