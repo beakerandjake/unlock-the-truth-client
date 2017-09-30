9ed23f61574ee38358aeec4c7846abe4e5af45f8
beakerandjake <jhoff462@gmail.com>
2017-09-30 16:16:01 -0700

    use question track module

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,8 +1,12 @@
 import angular from 'angular';
+
+// utt components
 import MainComponent from './app.component';
 import Header from './header/header.module';
 import Footer from './footer/footer.module';
+import QuestionTrack from './question-track/question-track.module';
 
+// Global styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const moduleName = 'unlock-the-truth';
@@ -12,7 +16,8 @@ const moduleName = 'unlock-the-truth';
 angular
     .module(moduleName, [
         Header,
-        Footer
+        Footer,
+        QuestionTrack
     ])
     .component('unlockTheTruth', MainComponent);
 
