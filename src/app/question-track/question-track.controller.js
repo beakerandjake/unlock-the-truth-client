1c3eefdb5a117178a176c1f27ab36cb0c4f7e10c
beakerandjake <jhoff462@gmail.com>
2017-09-30 16:51:28 -0700

    user service

--- src/app/question-track/question-track.controller.js
+++ src/app/question-track/question-track.controller.js
@@ -1,10 +1,22 @@
 class QuestionTrackController {
-    constructor() {
-        this.message = 'Hello World';
+    constructor(questionTrackService) {
+        'ngInject';
+
+        // Members
+        this._questionTrackService = questionTrackService;
+
+        // Properties
+        this.model = {};
     }
 
     $onInit() {
-
+        this._questionTrackService.getQuestions()
+            .then(result => {
+                console.log('Got a great result!', result);
+            })
+            .catch(() => {
+                console.log('Error loading questions!');
+            });
     }
 }
 
