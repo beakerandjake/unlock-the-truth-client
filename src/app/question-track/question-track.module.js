f0f6ab1a0407bd52196218c5b6b37a99d3dcdeed
beakerandjake <jhoff462@gmail.com>
2017-09-30 18:03:17 -0700

    use unanswered question module

--- src/app/question-track/question-track.module.js
+++ src/app/question-track/question-track.module.js
@@ -1,11 +1,14 @@
 import angular from 'angular';
+import UnansweredQuestion from './unanswered-question/unanswered-question.module';
 import questionTrackComponent from './question-track.component';
 import questionTrackService from './question-track.service';
 
 const moduleName = 'unlock-the-truth.question-track';
 
 angular
-    .module(moduleName, [])
+    .module(moduleName, [
+        UnansweredQuestion
+    ])
     .component('uttQuestionTrack', questionTrackComponent)
     .service('questionTrackService', questionTrackService);
 
