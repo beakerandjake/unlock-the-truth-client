62952c2af6c5585d13c9de0fe480f6ba26756227
beakerandjake <jhoff462@gmail.com>
2017-10-01 11:35:01 -0700

    use unlocked question module

--- src/app/question-track/question-track.module.js
+++ src/app/question-track/question-track.module.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import LockedQuestion from './locked-question/locked-question.module';
 import CurrentQuestion from './current-question/current-question.module';
+import UnlockedQuestion from './unlocked-question/unlocked-question.module';
 import questionTrackComponent from './question-track.component';
 import questionTrackService from './question-track.service';
 
@@ -9,7 +10,8 @@ const moduleName = 'unlock-the-truth.question-track';
 angular
     .module(moduleName, [
         LockedQuestion,
-        CurrentQuestion
+        CurrentQuestion,
+        UnlockedQuestion
     ])
     .component('uttQuestionTrack', questionTrackComponent)
     .service('questionTrackService', questionTrackService);
