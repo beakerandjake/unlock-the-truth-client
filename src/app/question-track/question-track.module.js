45e4e106898f3211101d2c18b7977663acab0109
beakerandjake <jhoff462@gmail.com>
2017-10-01 11:30:18 -0700

    use current question

--- src/app/question-track/question-track.module.js
+++ src/app/question-track/question-track.module.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import LockedQuestion from './locked-question/locked-question.module';
+import CurrentQuestion from './current-question/current-question.module';
 import questionTrackComponent from './question-track.component';
 import questionTrackService from './question-track.service';
 
@@ -7,7 +8,8 @@ const moduleName = 'unlock-the-truth.question-track';
 
 angular
     .module(moduleName, [
-        LockedQuestion
+        LockedQuestion,
+        CurrentQuestion
     ])
     .component('uttQuestionTrack', questionTrackComponent)
     .service('questionTrackService', questionTrackService);
