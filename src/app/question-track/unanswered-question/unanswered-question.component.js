3ade0b6e13597b7af30c371133be29c6f639bd99
beakerandjake <jhoff462@gmail.com>
2017-09-30 18:03:09 -0700

    add unanswered question module

---
+++ src/app/question-track/unanswered-question/unanswered-question.component.js
@@ -0,0 +1,10 @@
+import template from './unanswered-question.html';
+import controller from './unanswered-question.controller';
+
+const UnansweredQuestionComponent = {
+  bindings: {},
+  template,
+  controller
+};
+
+export default UnansweredQuestionComponent;
---
+++ src/app/question-track/unanswered-question/unanswered-question.controller.js
@@ -0,0 +1,11 @@
+class UnansweredQuestionController {
+    constructor() {
+        
+    }
+
+    $onInit() {
+
+    }
+}
+
+export default UnansweredQuestionController;
