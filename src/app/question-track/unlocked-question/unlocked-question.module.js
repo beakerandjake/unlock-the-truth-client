3b30c849a9f6d85a27cd1bc6932ae880d6de4fd6
beakerandjake <jhoff462@gmail.com>
2017-10-01 12:27:22 -0700

    add angular ui bootstrap

--- src/app/question-track/unlocked-question/unlocked-question.module.js
+++ src/app/question-track/unlocked-question/unlocked-question.module.js
@@ -1,10 +1,14 @@
 import angular from 'angular';
+import collapse from 'angular-ui-bootstrap/src/collapse';
+
 import unlockedQuestionComponent from './unlocked-question.component';
 
 const moduleName = 'unlock-the-truth.unlocked-question';
 
 angular
-    .module(moduleName, [])
+    .module(moduleName, [
+        collapse
+    ])
     .component('uttUnlockedQuestion', unlockedQuestionComponent);
 
 export default moduleName;
