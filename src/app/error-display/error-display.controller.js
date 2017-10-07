32be4f15726d4e1cab8d72ee88718e8cbf059556
beakerandjake <jhoff462@gmail.com>
2017-10-07 16:51:22 -0700

    add error display module

---
+++ src/app/error-display/error-display.controller.js
@@ -0,0 +1,11 @@
+import img from './error.jpg';
+
+// Controller for the error display component. 
+
+class ErrorDisplayController {
+    constructor() {
+        this.pathToErrorImage = img;
+    }
+}
+
+export default ErrorDisplayController;
