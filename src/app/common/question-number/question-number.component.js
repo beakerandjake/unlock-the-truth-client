0794ad8197e4fb38bc325ada862f6d74e9bbb82d
beakerandjake <jhoff462@gmail.com>
2017-10-07 11:40:49 -0700

    add some validation to question-number

--- src/app/common/question-number/question-number.component.js
+++ src/app/common/question-number/question-number.component.js
@@ -1,12 +1,14 @@
+import controller from './question-number.controller';
 import './question-number.css';
 
 // Component which handles displaying a questions number 
 
 const questionNumberComponent = {
-  bindings: {
-      number: '<qnNumber'
-  },
-  template: `<span ng-if="$ctrl.number" class="question-number">{{$ctrl.number}}</span>`
+    bindings: {
+        number: '@qnNumber'
+    },
+    template: `<span ng-if="$ctrl.visible" class="question-number">{{$ctrl.number}}</span>`,
+    controller
 };
 
 export default questionNumberComponent;
