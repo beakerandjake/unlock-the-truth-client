9d73323c2b4787608ecd816b9f70290a000d9ade
beakerandjake <jhoff462@gmail.com>
2017-10-07 15:17:52 -0700

    handle callback

--- src/app/question-track/current-question/current-question.controller.js
+++ src/app/question-track/current-question/current-question.controller.js
@@ -2,11 +2,16 @@
 
 class CurrentQuestionController {
     constructor() {
-        
+
     }
 
     $onInit() {
 
+    }
+
+    // Fired when one of our question type components raises their submit callback.  
+    onSubmit(answer) {
+        console.log(`Current question was answered with: ${answer}`);
     }
 }
 
