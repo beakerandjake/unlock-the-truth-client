6ac649224d017306d24e4cafa2273a6a56a9bacb
beakerandjake <jhoff462@gmail.com>
2017-10-07 11:41:16 -0700

    import question number

--- src/app/question-track/current-question/current-question.module.js
+++ src/app/question-track/current-question/current-question.module.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import collapsiblePanel from '../../common/collapsible-panel/collapsible-panel.module';
+import questionNumber from '../../common/question-number/question-number.module';
 import currentQuestionComponent from './current-question.component';
 
 const moduleName = 'unlock-the-truth.current-question';
 
 angular
     .module(moduleName, [
-        collapsiblePanel
+        collapsiblePanel,
+        questionNumber
     ])
     .component('uttCurrentQuestion', currentQuestionComponent);
 
--- src/app/question-track/locked-question/locked-question.module.js
+++ src/app/question-track/locked-question/locked-question.module.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import collapsiblePanel from '../../common/collapsible-panel/collapsible-panel.module';
+import questionNumber from '../../common/question-number/question-number.module';
 import lockedQuestionComponent from './locked-question.component';
 
 const moduleName = 'unlock-the-truth.locked-question';
 
 angular
     .module(moduleName, [
-        collapsiblePanel
+        collapsiblePanel,
+        questionNumber
     ])
     .component('uttLockedQuestion', lockedQuestionComponent);
 
