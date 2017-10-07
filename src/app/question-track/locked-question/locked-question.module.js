1c47c9845515fdac308bf3bad08f51efa25e9f56
beakerandjake <jhoff462@gmail.com>
2017-10-07 13:19:03 -0700

    capitalize module names

--- src/app/question-track/locked-question/locked-question.module.js
+++ src/app/question-track/locked-question/locked-question.module.js
@@ -1,14 +1,14 @@
 import angular from 'angular';
-import collapsiblePanel from '../../common/collapsible-panel/collapsible-panel.module';
-import questionNumber from '../../common/question-number/question-number.module';
+import CollapsiblePanel from '../../common/collapsible-panel/collapsible-panel.module';
+import QuestionNumber from '../../common/question-number/question-number.module';
 import lockedQuestionComponent from './locked-question.component';
 
 const moduleName = 'unlock-the-truth.locked-question';
 
 angular
     .module(moduleName, [
-        collapsiblePanel,
-        questionNumber
+        CollapsiblePanel,
+        QuestionNumber
     ])
     .component('uttLockedQuestion', lockedQuestionComponent);
 
