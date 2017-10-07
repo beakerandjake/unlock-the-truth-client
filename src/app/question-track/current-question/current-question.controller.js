c30729381900842af0941435bf5da8f234f64ef8
beakerandjake <jhoff462@gmail.com>
2017-10-07 15:54:24 -0700

    remove spinner from current question

--- src/app/question-track/current-question/current-question.controller.js
+++ src/app/question-track/current-question/current-question.controller.js
@@ -1,9 +1,7 @@
 // Controller for the current question component.  
 
 class CurrentQuestionController {
-    constructor() {
-        this.submittingAnswer = false;
-    }
+    constructor() {}
 
     $onInit() {
 
@@ -11,13 +9,6 @@ class CurrentQuestionController {
 
     // Fired when one of our question type components raises their submit callback.  
     onSubmit(answer) {
-        if (this.submittingAnswer) {
-            return false;
-        }
-
-        // TODO.. not sure if show busy status here yet..  
-        this.submittingAnswer = true;
-
         // Raise our callback 
         this.onAnswer({
             answer
