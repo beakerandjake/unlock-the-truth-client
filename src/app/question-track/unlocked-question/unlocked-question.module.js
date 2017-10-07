9022c717526419c899a783a69478492562610868
beakerandjake <jhoff462@gmail.com>
2017-10-07 13:34:10 -0700

    more comments

--- src/app/question-track/unlocked-question/unlocked-question.module.js
+++ src/app/question-track/unlocked-question/unlocked-question.module.js
@@ -3,6 +3,8 @@ import CollapsiblePanel from '../../common/collapsible-panel/collapsible-panel.m
 import QuestionNumber from '../../common/question-number/question-number.module';
 import unlockedQuestionComponent from './unlocked-question.component';
 
+// Module which contains an unlocked question component.  
+
 const moduleName = 'unlock-the-truth.unlocked-question';
 
 angular
