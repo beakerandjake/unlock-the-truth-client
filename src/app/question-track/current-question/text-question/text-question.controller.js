ffeb77f067184968321264fa6d772965367b8b4f
beakerandjake <jhoff462@gmail.com>
2017-10-08 08:55:12 -0700

    bind to wrong answer event

--- src/app/question-track/current-question/text-question/text-question.controller.js
+++ src/app/question-track/current-question/text-question/text-question.controller.js
@@ -1,8 +1,21 @@
+import {
+    wrongAnswerEvent
+} from '../../question-track.constants';
+
 // Controller for the text-question component
 
 class TextQuestionController {
-    constructor() {
+    constructor($scope) {
+        'ngInject';
+
+        // Bind to the wrong answer event so we can update our display.  
+        $scope.$on(wrongAnswerEvent, () => {
+            this.onIncorrectAnswer();
+        });
+
+        // Properties 
         this.answer = '';
+        this.answerIncorrect = false;
     }
 
     // Fired when the user clicks the submit button.
@@ -16,6 +29,13 @@ class TextQuestionController {
         this.onSubmit({
             answer: this.answer
         });
+    }
+
+    // Handle when the user answers incorrectly.
+    onIncorrectAnswer() {
+        console.log(this);
+        console.log('Incorrect!');
+        this.answerIncorrect = true;
     }
 }
 
