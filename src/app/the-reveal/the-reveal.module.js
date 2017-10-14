68cafdeb346903b6925f14f0fce35fca2106469e
beakerandjake <jhoff462@gmail.com>
2017-10-14 14:23:52 -0700

    register service

--- src/app/the-reveal/the-reveal.module.js
+++ src/app/the-reveal/the-reveal.module.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
+import theRevealService from './the-reveal.service';
 import theRevealComponent from './the-reveal.component';
 
 const moduleName = 'unlock-the-truth.the-reveal';
 
 angular
     .module(moduleName, [])
+    .service('theRevealService', theRevealService)
     .component('uttTheReveal', theRevealComponent);
 
 export default moduleName;
