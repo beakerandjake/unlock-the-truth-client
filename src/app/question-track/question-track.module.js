b28c90ed45fa167db7fc86e5f31c3ff294e21711
beakerandjake <jhoff462@gmail.com>
2017-10-14 12:06:29 -0700

    dont use module

--- src/app/question-track/question-track.module.js
+++ src/app/question-track/question-track.module.js
@@ -4,7 +4,6 @@ import ErrorDisplay from '../error-display/error-display.module';
 import LockedQuestion from './locked-question/locked-question.module';
 import CurrentQuestion from './current-question/current-question.module';
 import UnlockedQuestion from './unlocked-question/unlocked-question.module';
-import CorrectAnswer from './correct-answer/correct-answer.module';
 import questionTrackComponent from './question-track.component';
 import questionTrackService from './question-track.service';
 
@@ -18,8 +17,7 @@ angular
         ErrorDisplay,
         LockedQuestion,
         CurrentQuestion,
-        UnlockedQuestion,
-        CorrectAnswer
+        UnlockedQuestion
     ])
     .component('uttQuestionTrack', questionTrackComponent)
     .service('questionTrackService', questionTrackService);
