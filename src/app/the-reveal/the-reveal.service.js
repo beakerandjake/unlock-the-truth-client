0fffd1fd9dc54a1c6ee3e58aa9307e6bce56155c
beakerandjake <jhoff462@gmail.com>
2017-10-14 14:24:08 -0700

    mock unlock the truth

--- src/app/the-reveal/the-reveal.service.js
+++ src/app/the-reveal/the-reveal.service.js
@@ -1,12 +1,24 @@
 class TheRevealService {
-    constructor() {
+    constructor($q, $timeout) {
+        "ngInject";
 
+        // Members
+        this._$q = $q;
+        this._$timeout = $timeout;
     }
 
     // Returns a promise that is resolved with the result of the api. 
     // Returns the final truth if it's unlocked, otherwise returns nothing..
     unlockTheTruth() {
+        const deferred = this._$q.defer();
 
+        this._$timeout(() => {
+            deferred.resolve({
+                unlocked: false
+            })
+        }, 1000);
+
+        return deferred.promise;
     }
 }
 
