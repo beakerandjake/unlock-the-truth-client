b3e9aaff4a6d9c7871dae36c7b4c37512b0b0f91
beakerandjake <jhoff462@gmail.com>
2017-10-14 15:44:48 -0700

    comment

--- src/app/question-track/the-reveal/locked-reveal/locked-reveal.component.js
+++ src/app/question-track/the-reveal/locked-reveal/locked-reveal.component.js
@@ -1,6 +1,6 @@
 import template from './locked-reveal.html';
 
-// Component which displays the truth when it hasn't been unlocked.
+// Component which displays the truth when the user hasn't completed all the questions.
 
 const LockedRevealComponent = {
   bindings: {},
--- src/app/question-track/the-reveal/locked-reveal/locked-reveal.module.js
+++ src/app/question-track/the-reveal/locked-reveal/locked-reveal.module.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import lockedRevealComponent from './locked-reveal.component';
 
+// Module which contains the locked reveal component.  
+
 const moduleName = 'unlock-the-truth.the-reveal.locked';
 
 angular
