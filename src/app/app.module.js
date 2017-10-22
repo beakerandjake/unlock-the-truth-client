48b6cc8b8af478d3fc236763ab01841484e49ba8
beakerandjake <jhoff462@gmail.com>
2017-10-22 12:08:01 -0700

    remove usage

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -8,7 +8,6 @@ import 'font-awesome/css/font-awesome.min.css';
 import MainComponent from './app.component';
 import Header from './header/header.module';
 import Footer from './footer/footer.module';
-import Navbar from './navbar/navbar.module';
 import QuestionTrack from './question-track/question-track.module';
 
 const moduleName = 'unlock-the-truth';
@@ -19,8 +18,7 @@ angular
     .module(moduleName, [
         Header,
         Footer,
-        QuestionTrack,
-        Navbar
+        QuestionTrack
     ])
     .component('unlockTheTruth', MainComponent);
 
