f59656bc697056f81876878690757e0aecf48e64
beakerandjake <jhoff462@gmail.com>
2017-10-22 14:39:55 -0700

    add comment

--- src/app/common/authentication/authentication.module.js
+++ src/app/common/authentication/authentication.module.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import authenticationService from './authentication.service';
 
+// Module which contains authentication related components and services.  
+
 const moduleName = 'unlock-the-truth.authentication';
 
 angular
