e1014b7a3959b237b0a7b91491c4b93ff55bfc0f
beakerandjake <jhoff462@gmail.com>
2017-10-22 15:41:50 -0700

    wip store token method

--- src/app/common/authentication/authentication.service.js
+++ src/app/common/authentication/authentication.service.js
@@ -1,3 +1,5 @@
+
+
 // Service which handles authentication.  
 
 class AuthenticationService {
@@ -14,12 +16,17 @@ class AuthenticationService {
         console.log('USER', username, 'PASSWORD', password);
 
         const deferred = this._$q.defer();
-        
+
         this._$timeout(() => {
             deferred.reject('The username or password was incorrect.');
         }, 1000);
 
         return deferred.promise;
+    }
+
+    // Store the token from the API in the users local storage. 
+    _storeToken(token) {
+        console.log('asked to store token', token);
     }
 }
 
