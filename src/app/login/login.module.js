3f74e0775217c2a74555e34ba59c6b23c0e0f99f
beakerandjake <jhoff462@gmail.com>
2017-10-22 14:37:50 -0700

    use module

--- src/app/login/login.module.js
+++ src/app/login/login.module.js
@@ -1,10 +1,13 @@
 import angular from 'angular';
+import Authentication from '../common/authentication/authentication.module';
 import loginComponent from './login.component';
 
 const moduleName = 'unlock-the-truth.login';
 
 angular
-    .module(moduleName, [])
+    .module(moduleName, [
+        Authentication
+    ])
     .component('uttLogin', loginComponent);
 
 export default moduleName;
