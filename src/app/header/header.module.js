f3c92a2c02ecfe39a4bfc52d375ffaf928493de8
beakerandjake <jhoff462@gmail.com>
2017-10-23 17:34:28 -0700

    use auth module, show login/logout button

--- src/app/header/header.module.js
+++ src/app/header/header.module.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import UibModal from 'angular-ui-bootstrap/src/modal';
 import Login from '../common/authentication/login/login.module';
+import Authentication from '../common/authentication/authentication.module';
 import component from './header.component';
 
 // Module which contains the header component. 
@@ -10,7 +11,8 @@ const moduleName = 'unlock-the-truth.header';
 angular
     .module(moduleName, [
         UibModal,
-        Login
+        Login,
+        Authentication
     ])
     .component('uttHeader', component);
 
