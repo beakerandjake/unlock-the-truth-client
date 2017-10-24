9548c303e9183b258fb4c0383f0e193c6aeb55e7
beakerandjake <jhoff462@gmail.com>
2017-10-23 17:48:55 -0700

    logout component first pass

--- src/app/common/authentication/logout/logout.controller.js
+++ src/app/common/authentication/logout/logout.controller.js
@@ -4,14 +4,30 @@ class LogoutController {
     constructor(uttAuthenticationService) {
         'ngInject';
 
+        // Members
         this._authenticationService = uttAuthenticationService;
+
+        // Properties
+        this.busy = false;
     }
 
     // Fired when the user asks to logout. 
     logout() {
+        if (this.busy) {
+            return;
+        }
+
+        this.busy = true;
+
         this._authenticationService.logout()
             .then(() => {
                 this.close({});
+            })
+            .catch(() => {
+                //TODO HANDLE ERROR
+            })
+            .finally(() => {
+                this.busy = false;
             });
     }
 }
