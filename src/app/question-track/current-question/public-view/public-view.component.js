b57c4a405683fc677278a97240cbf1ab52c58b4e
beakerandjake <jhoff462@gmail.com>
2017-10-27 19:39:26 -0700

    add public view component

---
+++ src/app/question-track/current-question/public-view/public-view.component.js
@@ -0,0 +1,10 @@
+import template from './public-view.html';
+
+// Component which displays the current question to a non authenticated user. 
+
+const PublicViewComponent = {
+  bindings: {},
+  template
+};
+
+export default PublicViewComponent; 
