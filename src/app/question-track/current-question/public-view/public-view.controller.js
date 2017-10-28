68a04a5e20ee748025008de7dce26043d0a63385
beakerandjake <jhoff462@gmail.com>
2017-10-27 20:27:35 -0700

    show time since user unlocked.

--- src/app/question-track/current-question/public-view/public-view.controller.js
+++ src/app/question-track/current-question/public-view/public-view.controller.js
@@ -1,11 +1,36 @@
+import moment from 'moment';
+
 class PublicViewController {
-    constructor() {
-        
+    constructor($interval) {
+        'ngInject';
+
+        // Members
+        this._$interval = $interval;
+        this._updateInterval = null;
     }
 
     $onInit() {
-        console.log(this.question.timeUnlocked);
-        this.startTime = new Date(this.question.timeUnlocked);
+        this.calculateTimeSinceUnlock();
+
+        // Update the duration every minute so we always display the latest time.  
+        this._updateInterval = this._$interval(() => {
+            this.calculateTimeSinceUnlock();
+        }, 1000);
+    }
+
+    // Generate a human readable duration between now and when the user unlocked the question. 
+    calculateTimeSinceUnlock() {
+        const startTime = new moment(this.question.timeUnlocked);
+        const diff = moment().diff(startTime);
+        this.duration = moment.duration(diff).humanize();
+    }
+
+    // Be sure to cancel our interval when we get destroyed. 
+    $onDestroy() {
+        if (this._updateInterval) {
+            console.log('unsub');
+            this.$interval.cancel(this._updateInterval);
+        }
     }
 }
 
