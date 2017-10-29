47877cf23d19eb2715f2663a26cb15867ac910d0
beakerandjake <jhoff462@gmail.com>
2017-10-28 17:16:40 -0700

    add routes constant

--- src/app/question-track/question-track.constants.js
+++ src/app/question-track/question-track.constants.js
@@ -3,3 +3,9 @@ export const wrongAnswerEvent = 'utt.wrong-answer';
 
 // The event that gets broadcasted when the user answers correctly.  
 export const correctAnswerEvent = 'utt.correct-answer';
+
+// Defines the API routes relating to the question track.
+export const apiRoutes = {
+    getQuestions: '/questions',
+    answerQuestion: '/questions/:questionId'
+};
