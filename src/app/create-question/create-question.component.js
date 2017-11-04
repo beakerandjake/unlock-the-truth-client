12492f8ba898f2258eb49b7d5b4d4348b22c1498
beakerandjake <jhoff462@gmail.com>
2017-11-04 14:44:36 -0700

    wip create question

--- src/app/create-question/create-question.component.js
+++ src/app/create-question/create-question.component.js
@@ -1,5 +1,6 @@
 import template from './create-question.html';
 import controller from './create-question.controller';
+import './create-question.css';
 
 // Component which allows you to easily create a new question. 
 
