8cb5ba746999cd8b854cfbf01087c7ef73e0417e
bdell <bdell117@gmail.com>
2017-11-05 14:16:58 -0500

    small styling cleanup

--- src/app/question-track/unlocked-question/unlocked-question.controller.js
+++ src/app/question-track/unlocked-question/unlocked-question.controller.js
@@ -2,10 +2,6 @@
 import moment from 'moment';
 
 class UnlockedQuestionController {
-    constructor() {}
-
-    $onInit() {}
-
     timeTaken(question) {
         let { timeUnlocked, timeAnswered } = question;
         return moment(timeAnswered).diff(moment(timeUnlocked), 'hours');
