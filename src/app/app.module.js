0af3b08d88995f88c71f5352f9d0ddc0b2b41e73
beakerandjake <jhoff462@gmail.com>
2017-11-11 14:43:01 -0700

    use error display route

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -10,6 +10,7 @@ import UiRouterConfig from './configuration/ui-router.config';
 import MainComponent from './app.component';
 import Header from './header/header.module';
 import Footer from './footer/footer.module';
+import ErrorDisplay from './error-display/error-display.module';
 import CreateQuestion from './create-question/create-question.module';
 import QuestionTrack from './question-track/question-track.module';
 
@@ -23,6 +24,7 @@ angular
         UiRouter,
         Header,
         Footer,
+        ErrorDisplay,
         QuestionTrack,
         CreateQuestion
     ])
