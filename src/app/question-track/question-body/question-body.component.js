29784cdec2e9ace34cc13ee386b5eb1b4c9a1567
beakerandjake <jhoff462@gmail.com>
2017-11-18 19:40:36 -0700

    fix binding/import

--- src/app/question-track/question-body/question-body.component.js
+++ src/app/question-track/question-body/question-body.component.js
@@ -1,14 +1,13 @@
 import template from './question-body.html';
-import controller from './';
-import 'question-body.css';
+import './question-body.css';
 
 // Handles common scenario of binding a questions body html
 
 const QuestionBodyComponent = {
   bindings: {
-      body: '<body'
+      body: '<qbBody'
   },
   template
 };
 
-export default ComponentName;
+export default QuestionBodyComponent;
