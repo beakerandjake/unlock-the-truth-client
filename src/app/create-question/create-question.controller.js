41933ca6f7f02fd03ecceda0c168e93fb6bed1bf
beakerandjake <jhoff462@gmail.com>
2017-11-19 08:10:04 -0700

    finish "editor"

--- src/app/create-question/create-question.controller.js
+++ src/app/create-question/create-question.controller.js
@@ -20,6 +20,16 @@ class CreateQuestionController {
                 '_id': '12345',
                 'number': 666,
                 'timeUnlocked': '2017-11-19T02:45:21.712Z'
+            },
+            unlocked: {
+                '_id': '5a10eb56590a8d6d4b6fab8f',
+                'timeUnlocked': '2017-11-19T02:25:14.706Z',
+                'number': 1,
+                'timeAnswered': '2017-11-19T02:44:29.207Z',
+                'failedAttempts': 17
+            },
+            locked: {
+                number: 666
             }
         };
         this.questionTypes = constants.questionTypes;
@@ -58,6 +68,20 @@ class CreateQuestionController {
         this.projections.current.type = this.model.type;
 
         return this.projections.current;
+    }
+
+    getUnlockedQuestionModel() {
+        this.projections.unlocked.title = this.model.title;
+        this.projections.unlocked.unlockedBody = this.model.unlockedBody;
+        this.projections.unlocked.answer = this.model.answer;
+
+        return this.projections.unlocked;
+    }
+
+    getLockedQuestionModel() {
+        this.projections.locked.title = this.model.title;
+
+        return this.projections.locked;
     }
 }
 
