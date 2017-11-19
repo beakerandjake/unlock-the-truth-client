949631ed4bee7c4f21556b02317fc89458dc8023
beakerandjake <jhoff462@gmail.com>
2017-11-18 19:40:41 -0700

    use module

--- src/app/question-track/current-question/text-question/text-question.module.js
+++ src/app/question-track/current-question/text-question/text-question.module.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import AngularAnimate from 'angular-animate';
 import Insults from '../../../common/insults/insults.module';
+import QuestionBody from '../../question-body/question-body.module';
 import textQuestionComponent from './text-question.component';
 
 // Modules which contains the text-question component
@@ -10,7 +11,8 @@ const moduleName = 'unlock-the-truth.current-question.text';
 angular
     .module(moduleName, [
         AngularAnimate,
-        Insults
+        Insults,
+        QuestionBody
     ])
     .component('uttTextQuestion', textQuestionComponent);
 
