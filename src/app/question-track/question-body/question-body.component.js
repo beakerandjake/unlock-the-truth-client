0d1f1a9efc48ab01319674fa1d65c471e672ab11
beakerandjake <jhoff462@gmail.com>
2017-11-18 19:36:14 -0700

    add module

---
+++ src/app/question-track/question-body/question-body.component.js
@@ -0,0 +1,14 @@
+import template from './question-body.html';
+import controller from './';
+import 'question-body.css';
+
+// Handles common scenario of binding a questions body html
+
+const QuestionBodyComponent = {
+  bindings: {
+      body: '<body'
+  },
+  template
+};
+
+export default ComponentName;
