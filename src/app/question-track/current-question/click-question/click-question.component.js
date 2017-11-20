1b366176c1c10da9ea0576650136ee86245ac5a2
beakerandjake <jhoff462@gmail.com>
2017-11-19 18:53:03 -0700

    use style

--- src/app/question-track/current-question/click-question/click-question.component.js
+++ src/app/question-track/current-question/click-question/click-question.component.js
@@ -1,5 +1,6 @@
 import template from './click-question.html';
 import controller from './click-question.controller';
+import './click-question.css';
 
 // question which can only be answered by clicking a button X number of times. 
 
