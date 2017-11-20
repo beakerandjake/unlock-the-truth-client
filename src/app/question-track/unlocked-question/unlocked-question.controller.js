cf0d64faacc108eb87eebb7466fd33ec2a849ebe
bdell <bdell117@gmail.com>
2017-11-19 20:11:04 -0500

    adds string display of their failings

--- src/app/question-track/unlocked-question/unlocked-question.controller.js
+++ src/app/question-track/unlocked-question/unlocked-question.controller.js
@@ -4,11 +4,12 @@ import moment from 'moment';
 class UnlockedQuestionController {
     timeTaken(question) {
         const { timeUnlocked, timeAnswered } = question;
-        const timeDifference = moment(timeAnswered).diff(
-            moment(timeUnlocked),
-            'hours'
-        );
+        const timeDifference = moment(timeAnswered).diff(moment(timeUnlocked), 'hours');
         return moment.duration(timeDifference, 'hours').humanize();
+    }
+    completionText(question) {
+        const conditionalAttemptString = question.failedAttempts > 1 ? `and ${question.failedAttempts} attempts` : ``;
+        return `It took Eric and Jennie ${this.timeTaken(question)} ${conditionalAttemptString} to answer correctly.`;
     }
 }
 
